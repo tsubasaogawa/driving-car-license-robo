019b400a17258b86102673031e861bb9e127353a
Tsubasa Ogawa <hurikake@gmail.com>
2015-02-15 01:47:51 +0900

    add a title image

--- app/controllers/robo_controller.rb
+++ app/controllers/robo_controller.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 
 RESULT = {'1.0' => '○', '-1.0' => '×'}
+RESULT_PN = {'1.0' => '_positive', '-1.0' => '_negative'}
 
 class RoboController < ApplicationController
   def index
@@ -8,24 +9,30 @@ class RoboController < ApplicationController
   end
   
   def analyze
-    if params['input'] == nil
+    @result_pn = ''
+    if params['input']['user'] == ''
 	  @system_output = '文字が入力されていないよ'
 	  @morpho_result = nil
 	else
       @text = params['input']['user']
       analyser = MorphologicalAnalyzer.new(@text)
       @morpho_result = analyser.analyze
-      @morpho_result.each do |e|
-        puts "word = #{e['word']}, id = #{e['id']}, count = #{e['count']}"
+      if @morpho_result == nil
+        @system_output = '形態素解析に失敗したよ'
+      else
+        @morpho_result.each do |e|
+          puts "word = #{e['word']}, id = #{e['id']}, count = #{e['count']}"
+        end
+
+        svmline = SvmFeatureCreator.new(analyser.features)
+        svmline.create
+
+        pred = SvmPredictor.new(svmline.svm_line)
+        result = pred.predict
+        @system_output = RESULT["#{result}"]
+        @result_pn = RESULT_PN["#{result}"]
       end
-
-      svmline = SvmFeatureCreator.new(analyser.features)
-      svmline.create
-
-      pred = SvmPredictor.new(svmline.svm_line)
-      result = pred.predict
-      @system_output = RESULT["#{result}"]
     end
-    puts @system_output
+    puts "#{result} / #{@system_output}"
   end
 end
--- lib/bag_of_words.rb
+++ lib/bag_of_words.rb
@@ -14,13 +14,18 @@ class BoW
       results: 'ma,uniq',
       uniq_filter: UNIQ_FILTER
     })
-    result['ResultSet']['uniq_result']['word_list']['word'].each do |word_hash|
-      if id = @dict_array.index(word_hash['surface'])
-        output_hash["#{id}"] = word_hash['count']
-        @words.push({'word' => word_hash['surface'], 'count' => word_hash['count'], 'id' => id})
-	 else
-        @words.push({'word' => word_hash['surface'], 'count' => word_hash['count'], 'id' => -1})
-      end	 
+    if result['ResultSet']['uniq_result']['word_list'] == nil
+      output_hash[0] = nil
+      @words = nil
+    else
+      result['ResultSet']['uniq_result']['word_list']['word'].each do |word_hash|
+        if id = @dict_array.index(word_hash['surface'])
+          output_hash["#{id}"] = word_hash['count']
+          @words.push({'word' => word_hash['surface'], 'count' => word_hash['count'], 'id' => id})
+        else
+          @words.push({'word' => word_hash['surface'], 'count' => word_hash['count'], 'id' => -1})
+        end	 
+      end
     end
     return output_hash.sort{|a, b| a[0].to_i <=> b[0].to_i}
   end
